0a7c786cc8a9d5b349f5dd33c0abc784ef7b2590
ttlbyte <ruansq@smail.nju.edu.cn>
2015-08-30 18:36:30 +0800

    fix bug while reading a empty line

--- aims2abacus.py
+++ aims2abacus.py
@@ -14,12 +14,16 @@ vectorlist=[]
 atomtype=[]
 atom_coor=[]
 atom_coor_full=[]
+a2b=1.88972666
 with open('geometry.in','r') as fin:
     line=fin.readline();
     while(len(line)>0):
         while(line[0]==' '):
-            line=line[1:]       #ingore white spaces at the begining of lines
-        if line[0]=='#':
+            line=line[1:]
+        if len(line)<=1:
+            line=fin.readline();
+            continue;
+        if line[0]=='#':      #ingore white spaces at the begining of lines
             pass;
         if line.split()[0]=='lattice_vector':
             vectorlist.append([line.split()[1],line.split()[2],line.split()[3]])
@@ -27,15 +31,15 @@ with open('geometry.in','r') as fin:
         if line.split()[0]=='constrain_relaxation':
             constrain=line.split()[1];
             if constrain=='.true.':
-                atom_coor[-1][3]=0;
-                atom_coor[-1][4]=0;
-                atom_coor[-1][5]=0;
+                atom_coor[-1][3]='0';
+                atom_coor[-1][4]='0';
+                atom_coor[-1][5]='0';
             if constrain=='x':
-                atom_coor[-1][3]=0;
+                atom_coor[-1][3]='0';
             if constrain=='y':
-                atom_coor[-1][4]=0;
+                atom_coor[-1][4]='0';
             if constrain=='z':
-                atom_coor[-1][5]=0;
+                atom_coor[-1][5]='0';
             else:
                 pass;
         if line.split()[0]=='atom_frac':
@@ -46,7 +50,7 @@ with open('geometry.in','r') as fin:
                     atom_coor=[];
                 atomtype.append(atomname);
                 j+=1;
-            atom_coor.append([line.split()[1],line.split()[2],line.split()[3],1,1,1]);
+            atom_coor.append([line.split()[1],line.split()[2],line.split()[3],'1','1','1']);
             l+=1;
         if line.split()[0]=='atom':
             k=1;
@@ -57,24 +61,24 @@ with open('geometry.in','r') as fin:
                     atom_coor=[];
                 atomtype.append(atomname);
                 j+=1;
-            atom_coor.append([line.split()[1],line.split()[2],line.split()[3]]);
+            atom_coor.append([line.split()[1],line.split()[2],line.split()[3],'1','1','1']);
         line=fin.readline();
 atom_coor_full.append(atom_coor);
 #store the coordinates of atoms
 l=0;
 with open('STRU','w') as fout:
     fout.write('LATTICE_CONSTANT\n');
-    fout.write('1.88972666   //add lattice constant(a.u.)\n')
+    fout.write('1   //add lattice constant(a.u.)\n')
     while (i>=1):
-        fout.write(vectorlist[l][0]+' '+vectorlist[l][1]+' '+vectorlist[l][2]+'\n');
+        fout.write(str("%0.8f" % (float(vectorlist[l][0])*a2b))+' '+str("%0.8f" % (float(vectorlist[l][1])*a2b))+' '+str("%0.8f" % (float(vectorlist[l][2])*a2b))+'\n');
         l+=1;
         i-=1;
     fout.write('\n');
     fout.write('ATOMIC_POSITIONS\n');
     if k==0:
-        fout.write('Direct\n');
+        fout.write('Direct   //Direct or Cartesian coordinate\n');
     else:
-        fout.write('Cartesian\n');
+        fout.write('Cartesian    //Direct or Cartesian coordinate\n');
     fout.write('\n');
     m=len(atomtype)-1;
     while(m>=0):
@@ -85,6 +89,6 @@ with open('STRU','w') as fout:
         fout.write(str(i)+'    //number of atoms\n');
         i-=1;
         while(i>=0):
-            fout.write(atom_coor_full[m][i][0]+' '+atom_coor_full[m][i][1]+' '+atom_coor_full[m][i][2]+' 0 0 0\n')
+            fout.write(atom_coor_full[m][i][0]+' '+atom_coor_full[m][i][1]+' '+atom_coor_full[m][i][2]+' '+atom_coor_full[m][i][3]+' '+atom_coor_full[m][i][4]+' '+atom_coor_full[m][i][5]+'\n')
             i-=1;
         m-=1;
